d986c989f762f66efa8d857737c6899e67f4f0a7
“Gayathiri” <gayathiri.asokan@ideas2it.com>
2021-06-09 22:54:08 +0530

    client config

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,40 +1,24 @@
-/**
- * 
- */
 package com.example.demo;
 
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Configuration;
 
-/**
- * DemoApplication is used for main application 
- * @EnableAspectJAutoProxy annotation enables support for handling the components marked with @Aspect annotation
- * @author ubuntu
- *
- */
-@SpringBootApplication 
 @EnableConfigServer
-@EnableSwagger2
+@SpringBootApplication(exclude = {
+	    DataSourceAutoConfiguration.class, 
+	    DataSourceTransactionManagerAutoConfiguration.class, 
+	    HibernateJpaAutoConfiguration.class
+	})
 public class DemoApplication {
-	
-	/**
-	 * Main is used to start the application
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}	
-	
-	@Bean
-	   public Docket productApi() {
-	      return new Docket(DocumentationType.SWAGGER_2).select()
-	         .apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
-	   }
+	}
+
 }
